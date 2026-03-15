799e13e9e475a25c3231f38f74567070d856f14d
mhmdanton <mhmdanton@example.com>
2026-03-16 02:55:14 +0700

    feat: integrate calculator into CodeIgniter 4 MVC structure

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('calculator', 'Calculator::index');
+$routes->post('calculator/calculate', 'Calculator::calculate');
